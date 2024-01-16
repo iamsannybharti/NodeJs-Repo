1e1e9a5af352b04490bc05d2bc5695da169c1758
iamsannybharti <84441854+iamsannybharti@users.noreply.github.com>
2024-01-16 21:59:57 +0530

    updated code

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const readline = require('readline');
 const fs = require('fs');
 const http = require('http');
+const { parse } = require('path');
+const url = require('url');
+// const events = require('events');
+const ReplaceHtml = require('./Moduls/RepaceHtml');
+const user = require('./Moduls/user');
 
 /*LECTURE 4: CODE EXAMPLE************
 READING INPUT & WRITING OUTPUT
@@ -45,42 +50,90 @@ rl.on('close', () => {
 // ***************************************
 const html = fs.readFileSync('./Template/index.html', 'utf-8');
 let products = JSON.parse(fs.readFileSync('./Data/Data.json', 'utf-8'));
+const ProductListhtml = fs.readFileSync('./Template/ProductList.html', 'utf-8');
+const ProductDetailhtml = fs.readFileSync('./Template/ProductDetail.html', 'utf-8');
+// const ProdcutListhtmlmodifyied = products.map((product) => {
+//     let output = ProductListhtml.replace('{{%IMAGE%}}', product.productImage);
+//     output = output.replace('{{%NAME%}}', product.name);
+//     output = output.replace('{{%MODELNAME%}}', product.modeName);
+//     output = output.replace('{{%MODELNO%}}', product.modelNumber);
+//     output = output.replace('{{%SIZE%}}', product.size);
+//     output = output.replace('{{%CAMERA%}}', product.camera);
+//     output = output.replace('{{%PRICE%}}', product.price);
+//     output = output.replace('{{%COLOR%}}', product.color);
+//     output = output.replace('{{%ID%}}', product.id);
+//     output = output.replace('{{%ROM%}}', product.ROM);
+//     output = output.replace('{{%DESC%}}', product.Description);
+//     return output;
+// });
 
-const server = http.createServer((request, response) => {
-    let path = request.url;
 
-    if (path === '/' || path.toLowerCase() === '/home') {
+// function ReplaceHtml(template, product) {
+//     let output = template.replace('{{%IMAGE%}}', product.productImage);
+//     output = output.replace('{{%NAME%}}', product.name);
+//     output = output.replace('{{%MODELNAME%}}', product.modeName);
+//     output = output.replace('{{%MODELNO%}}', product.modelNumber);
+//     output = output.replace('{{%SIZE%}}', product.size);
+//     output = output.replace('{{%CAMERA%}}', product.camera);
+//     output = output.replace('{{%PRICE%}}', product.price);
+//     output = output.replace('{{%COLOR%}}', product.color);
+//     output = output.replace('{{%ID%}}', product.id);
+//     output = output.replace('{{%ROM%}}', product.ROM);
+//     output = output.replace('{{%DESC%}}', product.Description);
+//     return output;
+// }
 
-        response.writeHead(200, {
-            'content-type': 'text/html',
-            'my-header': 'true',
-        });
-        response.end(html.replace('{{%CONTENT%}}', 'Welcome To Home'));
 
-    } else if (path.toLowerCase() === '/products') {
+// const server = http.createServer((request, response) => {
+//     let { query, pathname: path } = url.parse(request.url, true);
+//     // console.log(x);
+//     // let path = request.url;
 
-        response.writeHead(200, {
-            'content-type': 'application/json',
-        });
-        response.end(JSON.stringify(products));
+//     if (path === '/' || path.toLowerCase() === '/home') {
 
-    } else if (path.toLowerCase() === '/about') {
+//         response.writeHead(200, {
+//             'content-type': 'text/html',
+//             'my-header': 'true',
+//         });
+//         response.end(html.replace('{{%CONTENT%}}', 'Welcome To Home'));
 
-        response.writeHead(200);
-        response.end(html.replace('{{%CONTENT%}}', 'Welcome To About'));
+//     } else if (path.toLowerCase() === '/products') {
+//         if (query.id) {
+//           let  product= products[query.id];
+//           let ProdcutDetailhtmlmodifyied= ReplaceHtml(ProductDetailhtml,product)
+//             response.end(html.replace('{{%CONTENT%}}', ProdcutDetailhtmlmodifyied));
+//         } else {
+//             response.writeHead(200, {
+//                 'content-type': 'text/html',
+//             });
+//             let ProdcutListhtmlmodifyied = products.map((product) => {
+//                 return ReplaceHtml(ProductListhtml, product);
+//             });
+//             response.end(html.replace('{{%CONTENT%}}', ProdcutListhtmlmodifyied.join(',')));
+//         }
+//         // response.writeHead(200, {
+//         //     'content-type': 'application/json',
+//         // });
+//         // response.end(JSON.stringify(products));
 
-    } else if (path.toLowerCase() === '/contact') {
 
-        response.writeHead(200);
-        response.end(html.replace('{{%CONTENT%}}', 'Welcome To Contact'));
+//     } else if (path.toLowerCase() === '/about') {
 
-    } else {
+//         response.writeHead(200);
+//         response.end(html.replace('{{%CONTENT%}}', 'Welcome To About'));
 
-        response.writeHead(404);
-        response.end(html.replace('{{%CONTENT%}}', 'Page Not Found'));
-        
-    }
-});
+//     } else if (path.toLowerCase() === '/contact') {
+
+//         response.writeHead(200);
+//         response.end(html.replace('{{%CONTENT%}}', 'Welcome To Contact'));
+
+//     } else {
+
+//         response.writeHead(404);
+//         response.end(html.replace('{{%CONTENT%}}', 'Page Not Found'));
+
+//     }
+// });
 
 
 //step 2 : start server
@@ -89,6 +142,58 @@ const server = http.createServer((request, response) => {
 // server.listen(PORT, () => {
 //     console.log(`Server is running on port ${PORT}`);
 // });
+
+const server = http.createServer();
 server.listen(800, '127.0.0.1', () => {
     console.log("server stated");
+});
+
+
+
+// UNDERSTANDING EVENT DRIVEN ARCHITECTURE
+// ***************************************
+
+// let myEmitter = new user();
+// myEmitter.on('user created',(id,name)=>{
+//     console.log(`A new user ${name} with id ${id} created`);
+// });
+// myEmitter.on('user created',(id,name)=>{
+//     console.log(`A new user ${name} with id ${id} created`);
+// });
+
+// myEmitter.emit('user created',101,'sanny');
+
+// UNDERSTANDING STREAMS IN PRACTICE
+// ***************************************
+//SOLUTION 1: WITHOUT READABLE OR WRITABLE STREAM
+// server.on('request', (req, res) =>{
+//     fs.readFile('./files/LargeFile.txt', (err, data) =>{
+//         if(err){
+//             res.end('Something went wrong!');
+//             return;
+//         }
+//         res.end(data);
+//     })
+// });
+
+//SOLUTION 2: WITH READABLE OR WRITABLE STREAM
+// server.on('request', (req, res) =>{
+//     let rs= fs.createReadStream('./files/LargeFile.txt');
+
+//     rs.on('data',(chunk)=>{
+//         res.write(chunk);
+//     });
+//     rs.on('end',()=>{
+//         res.end();
+//     })
+//     rs.on('error',(error)=>{
+//         res.end(error.message);
+//     });
+// });
+
+//SOLUTION 2: Using Pipe Method
+
+server.on('request', (req, res) =>{
+    let rs= fs.createReadStream('./files/LargeFile.txt');
+    rs.pipe(res);
 });
